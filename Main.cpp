db17d4007f7c024438b4c189dd969024a381019f
kraksy <kraksy8@gmail.com>
2023-09-26 21:15:50 +0200

    learned few things

--- Main.cpp
+++ Main.cpp
@@ -31,7 +31,6 @@ const char* fragmentShaderSource = "#version 330 core\n"
 
 int main()
 {
-
     // init everything
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -52,17 +51,6 @@ int main()
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
-    }
-
-    while (!glfwWindowShouldClose(window))
-    {
-        processInput(window);
-
-        glClearColor(0.2f, 0.1f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
     }
 
     // creating shader
@@ -91,7 +79,6 @@ int main()
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(fragmentShader);
 
-
     // create shader program , its a multiple shaders combined
     unsigned int shaderProgram;
     shaderProgram = glCreateProgram();
@@ -108,33 +95,64 @@ int main()
         std::cout << "ERROR::shaderProgram::FAILED\n" << infoLog << std::endl;
     }
 
-    unsigned int VBO;
-
     float vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f,
-        0.0f,  0.5f, 0.0f
+     0.5f,  0.5f, 0.0f,  // top right
+     0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f, -0.5f, 0.0f,  // bottom left
+    -0.5f,  0.5f, 0.0f   // top left 
     };
+
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    unsigned int VBO, VAO, EBO;
+
+    // creating 
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    // EBO init
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // copy vertices array for openGL
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
 
     // copy vertices in buffer 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // set vertex attribute pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
-    // just use the shader program B] 
-    glUseProgram(shaderProgram);
+    while (!glfwWindowShouldClose(window))
+    {
+        processInput(window);
 
-    
+        // just use the shader program B] 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
     glfwTerminate();
     return 0;
 }
-
 
 // processing inputs
 void processInput(GLFWwindow* window)
