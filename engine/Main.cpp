46bcd611e9e9377a619baf7a1c28e7bf90a9d48a
kraksy <kraksy8@gmail.com>
2023-10-03 20:01:07 +0200

    ey

--- engine/Main.cpp
+++ engine/Main.cpp
@@ -4,6 +4,10 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+#include </Users/kraks/Desktop./GLfun/GLfun/sprites/stb_image.h>
+#include </Users/kraks/Desktop/GLfun/GLfun/engine/shaders/shader.h>
+#include </Users/kraks/Desktop/GLfun/GLfun/engine/glm/glm.hpp>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -13,11 +17,15 @@ int width = 1200;
 int main()
 {
     // init everything
+    // ..............................
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+
+    // error chech
+    // ................................
     GLFWwindow* window = glfwCreateWindow(width, height, "window", NULL, NULL);
     if (window == NULL)
     {
@@ -33,80 +41,106 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-  
-    // shaders
-    Shader ourShader("3.3.shader.vs", "3.3.shader.fs");
 
+    // shaders
+    // ..................................
+    Shader ourShader("shaders/3.3.shader.vs", "shaders/3.3.shader.fs");
+
+    // data 
+    // ..........................
     float vertices[] = {
-        // positions         // colors        // texture
        -0.5f, 0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  0.0f, 0.0f,
         0.5f, 0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
-        0.0f,-0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.5f, 1.0f   
+        0.0f,-0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.5f, 1.0f
     };
 
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    unsigned char* data = stbi_load("wall.jpg", &width, &height, &nrChannels, 0);
-
-    if (data)
+    struct Vertex
     {
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-    }
-    else
+        glm::vec3 position;
+        glm::vec4 color;
+    };
+
+    Vertex triangle[3] =
     {
-        std::cout << "Failed to load texture" << std::endl;
-    }
+        {glm::vec3(-0.5, 0.5f, 0.0f), glm::vec4(0.9f, 0.8f, 0.2f, 1.0f)},
+        {glm::vec3(-0.5, 0.5f, 0.0f), glm::vec4(0.9f, 0.8f, 0.2f, 1.0f)},
+        {glm::vec3(-0.5, 0.5f, 0.0f), glm::vec4(0.9f, 0.8f, 0.2f, 1.0f)}
+    };
 
-    stbi_image_free(data);
+    unsigned int myVaoid;
+    
+    glCreateVertexArrays(1, &myVaoid);
+    glBindVertexArray(myVaoid);
+    
+    unsigned int myVboid;
 
-    unsigned int VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &myVboid);
+    glBindBuffer(GL_ARRAY_BUFFER, myVboid);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangle), triangle, GL_STATIC_DRAW);
 
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    // binding vertex position
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
     glEnableVertexAttribArray(0);
 
-    // binding colours ? 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
     glEnableVertexAttribArray(1);
 
-    // texture something
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-    
+    // vertex buffers
+    // ..........................
 
+    // vertex buffer object = VBO
+    // vertex array object  = VAO
+    // element buffer object = EBO
+
+    //unsigned int VBO, VAO, EBO;
+
+    // bind vertex buffers
+    //glGenBuffers(1, &VBO);
+    //glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // send what we want to do with the specific buffers 
+    // GL_STATIC_DRAW
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // binding array buffers 
+    //glGenVertexArrays(1, &VAO); 
+    //glBindVertexArray(VAO);
+
+    // linking vertex attributes
+    // ...........................
+
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    //glEnableVertexAttribArray(0);
+
+    // binding colours ? 
+    //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(3 * sizeof(float)));
+    //glEnableVertexAttribArray(1);
+
+    // texture something
+    //glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)(6 * sizeof(float)));
+    //glEnableVertexAttribArray(2);
+
+    // loop
+    // .....................................
     while (!glfwWindowShouldClose(window))
     {
+        // inputs
         processInput(window);
 
+        // bg render
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBindTexture(GL_TEXTURE_2D, texture);
+        // render
         ourShader.use();
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
+        // window shit
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &myVaoid);
+    glDeleteBuffers(1, &myVboid);
 
     glfwTerminate();
     return 0;
--- engine/SpriteTexture.cpp
+++
@@ -1,1 +0,0 @@
-#include "SpriteTexture.h"
