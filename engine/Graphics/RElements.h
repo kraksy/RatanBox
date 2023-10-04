c7d8932596376755c5703aad9022c0c8abbb0337
kraksy <kraksy8@gmail.com>
2023-10-04 22:23:26 +0200

    meh , add something tommorow

--- engine/Graphics/RElements.h
+++ engine/Graphics/RElements.h
@@ -1,23 +1,29 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <iostream>
 
 
 class RElements
 {
 	public:
-		std::arra
+		struct Vertex
+		{
+			std::array<float, 4> color;
+			std::array<float, 3> position;
+		};
+
+		static std::array<Vertex, 6> challenge1Square = {
+			//      Color                         Position
+			Vertex{{0.9f,  0.1f, 0.12f, 1.0f},   {-0.5f, -0.5f, 0.0f}},
+			Vertex{{0.1f,  0.9f, 0.12f, 1.0f},   {-0.5f,  0.5f, 0.0f}},
+			Vertex{{0.12f, 0.9f, 0.1f,  1.0f},   { 0.5f,  0.5f, 0.0f}},
+
+			Vertex{{0.9f,  0.1f, 0.12f, 1.0f},   {-0.5f, -0.5f, 0.0f}},
+			Vertex{{0.12f, 0.9f, 0.1f,  1.0f},   { 0.5f,  0.5f, 0.0f}},
+			Vertex{{0.12f, 0.1f, 0.9f,  1.0f},   { 0.5f, -0.5f, 0.0f}}
+		};
 		
-
-		
-
-
-
-
-
-
-
-
 };
 
 
