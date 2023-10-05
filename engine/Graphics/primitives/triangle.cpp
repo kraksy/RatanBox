b87f1a055fbd345aed89ef0e76f284d6ea5718b0
marcel <kraksy8@gmail.com>
2023-10-05 13:52:08 +0200

    dd d"add SSss
    i
    C
    C
    C

--- engine/Graphics/primitives/triangle.cpp
+++ engine/Graphics/primitives/triangle.cpp
@@ -1,1 +1,19 @@
 #include "triangle.h"
+#include <glm/glm.hpp>
+
+namespace Graphics 
+{
+  namespace primitives 
+  {
+    struct Vertex {
+      std::array<float, 4> color;
+      std::array<float, 3> position;
+    };     
+
+    std::array<Vertex, 6> triangle = 
+    {
+      // vertexes for triangle
+    }
+
+  } 
+}
