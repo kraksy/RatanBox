0c493dc2e3d943eab4231e4a3cb049db61d38037
kraksy <kraksy8@gmail.com>
2023-10-07 16:34:51 +0200

    window creation class in works , gonna finish when my sanity better"

--- RatanBox/src/Application.cpp
+++ RatanBox/src/Application.cpp
@@ -1,13 +1,17 @@
 #include <core/core.h>
+#include <core/input.h>
+#include <core/Window.h>
 
-// always put header files in /core
+
+const int width = 850;
+const int height = 850;
+const char* title = "window";
 
 
 int main()
 {
-
-
-
+	glfwInit();
+	
 
 	return 0;
 }
