6d87d3edb85e4ed7e2519b3af122a79689c1c521
kraksy <kraksy8@gmail.com>
2023-12-27 16:27:17 +0100

    upgrade dir

--- build.bat
+++ build.bat
@@ -2,8 +2,15 @@
 
 set "project_dir=%~dp0"
 set "build_dir=%project_dir%\..\..\build"
+set "include_dir=%project_dir%\lib\include"
 
 mkdir "%build_dir%"
 pushd "%build_dir%"
-cl -Zi "%project_dir%\src\main.cpp" user32.lib Gdi32.lib /showIncludes /I "include"
+
+for /D %%d in ("%include_dir%\*") do (
+    set "INCLUDE=%%d;%INCLUDE%"
+    echo Added %%d to INCLUDE
+)
+
+cl -Zi "%project_dir%\src\main.cpp" user32.lib Gdi32.lib /showIncludes /I "%build_dir%\lib\%INCLUDE%"
 popd
--- shell.bat
+++ shell.bat
@@ -1,5 +1,5 @@
 @echo
 call "D:\apps\VS\VC\Auxiliary\Build\vcvarsall.bat" x64
 set path=src;%path%
-set include=include;%include%
-set lib=library;%library%
+set include=lib\include;%include%
+set lib=lib\library;%library%
