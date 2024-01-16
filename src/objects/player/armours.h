b557c434979a80f7961a1df9f2ecef746234c6ad
marcel <kraksy8@gmail.com>
2024-01-16 10:55:24 +0100

    idk

--- src/objects/player/armours.h
+++ src/objects/player/armours.h
@@ -7,6 +7,28 @@ class armours()
         float health;
 
         float armorPoints;
-    public: 
+        float dodgePoints;
         
+
+        // bonuses
+        //int str;
+        //int tol;
+
+    public: 
+        armours();
+        ~armours();
+
+        void create();
+
+        void changePos(int x , int y);
+         
+        
+
 }
+
+void armours::changePos(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
+
---
+++ src/utils/map.h
@@ -0,0 +1,13 @@
+class map()
+{
+    private:
+        int width;
+        int height; 
+
+        int xTiles;
+        int yTiles;
+
+        float rotation;
+    public:
+        int calculateTiles();
+}
